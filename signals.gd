8d7e2e05ff7598ca81d37a79ccee14744681d54b
Mickael Mogmi Bidon <mogmi95@gmail.com>
2024-10-05 19:10:48 +0200

    Added a global class for signals
    Added some custom signals

---
+++ signals.gd
@@ -0,0 +1,16 @@
+extends Node
+
+signal food_consumed(position: Vector2)
+
+signal minion_hurt(minion: Node, source: Node)
+
+signal boss_hurt()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
