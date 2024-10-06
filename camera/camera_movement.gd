159c3a37e61e040a9ac50bd71f4057f3a7412a2f
Spiderboy <contact@spiderboy.fr>
2024-10-06 15:23:29 +0200

    Where_to passed to camera (but not used)

--- camera/camera_movement.gd
+++ camera/camera_movement.gd
@@ -3,12 +3,15 @@ extends Node2D
 # Camera movement speed (pixels)
 @export var speed : float = 2.5
 var should_camera_move: bool
+var where_to_max : int = 0
 
 func _process(delta) -> void:
-    # TODO: Handle right limits of levels
+    delta = delta
     if should_camera_move:
         position.x += speed
 
-func set_movement_and_back_collision(should_move: bool) -> void:
+
+func set_movement_and_back_collision(should_move: bool, where_to: int) -> void:
     should_camera_move = should_move
+    where_to_max = where_to
     $Area2D/CollisionShape2D.set_deferred("disabled", not should_move)
