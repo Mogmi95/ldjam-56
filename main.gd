eaf9c275e9c20c48322f6af5d15dd8e70becca7c
Spiderboy <contact@spiderboy.fr>
2024-10-06 12:05:09 +0200

    Remove dead code

--- main.gd
+++ main.gd
@@ -16,11 +16,6 @@ func _process(delta: float) -> void:
 
 func _input(event):
     pass
-    # Mouse in viewport coordinates.
-    #if event is InputEventMouseButton:
-    #    print("Mouse Click/Unclick at: ", event.position)
-    #elif event is InputEventMouseMotion:
-    #    print("Mouse Motion at: ", event.position)
 
 
 func new_game() -> void:
--- player/player.gd
+++ player/player.gd
@@ -22,17 +22,11 @@ func _ready() -> void:
     clamp_x = false
 
 func _input(event):
-    # Mouse in viewport coordinates.
-    #if event is InputEventMouseButton:
-    #    print("Mouse Click/Unclick at: ", event.position)
     if event is InputEventMouseMotion:
         mouse_pos = event.position
         currently_used_controller = Controller.MOUSE
     else:
         currently_used_controller = Controller.KEYBOARD
-
-    # Print the size of the viewport.
-    #print("Viewport Resolution is: ", get_viewport().get_visible_rect().size)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
