e7186b31c13b15425933606a2ae6ea572e5af687
Spiderboy <contact@spiderboy.fr>
2024-10-07 19:14:42 +0200

    Fix camera movement

--- camera/camera_movement.gd
+++ camera/camera_movement.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 # Camera movement speed (pixels)
-@export var speed: float = 2.5
+@export var speed: float = 150
 
 var where_to: int
 
 func _process(delta) -> void:
     if position.x < where_to:
-        position.x += speed
+        position.x += speed * delta
 
 
 func set_destination_and_back_collision(should_kill: bool, destination: int) -> void:
