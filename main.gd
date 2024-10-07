e8f4707c59053a710e797e3486130f3570b8907e
Roulyo <roulyo.42@gmail.com>
2024-10-07 15:30:01 +0200

    adds unload of level adds level 3

--- main.gd
+++ main.gd
@@ -2,8 +2,10 @@ extends Node
 
 # Starts at 1
 var level_nbr: int = 0
+var precedent_level_sc: Level = null
 var current_level_sc: Level = null
 var x_offset: int = 0
+var dezoom_offset: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,12 +14,18 @@ func _ready() -> void:
     Signals.start_game.connect(new_game)
     Signals.mob_died.connect(_on_mob_died)
     $PostFightTimer.timeout.connect(_on_post_fight_timer_timeout)
+    $Player.hide()
     $HUD.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
     if current_level_sc != null:
         _update_hud()
+
+        if $CameraCollision.position.x >= (x_offset + current_level_sc.camera_where_to) && precedent_level_sc:
+            _unload_level(precedent_level_sc)
+            precedent_level_sc = null
+
         if $CameraCollision.position.x >= (x_offset + current_level_sc.loading_checkpoint):
             _change_level()
 
@@ -32,7 +40,9 @@ func _update_hud() -> void:
 
 func new_game() -> void:
     $MainMenu.hide()
+    $Player.show()
     $HUD.show()
+    Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
     current_level_sc = null
     $CameraCollision.position = Vector2.ZERO
     level_nbr = 0
@@ -45,20 +55,29 @@ func game_over() -> void:
 
 # Transitions between levels should be handled here
 func load_level(lvl_nbr: int) -> void:
-    print("Loading level %d" % level_nbr)
+    if precedent_level_sc:
+        _unload_level(precedent_level_sc)
 
     if current_level_sc:
         x_offset += current_level_sc.level_width
+        precedent_level_sc = current_level_sc
 
+    print("Loading level %d @x=%d" % [level_nbr, x_offset])
     current_level_sc = load("res://levels/level_%s.tscn" % lvl_nbr).instantiate()
 
-    $CameraCollision.set_destination_and_back_collision(current_level_sc.should_camera_move,
-                                                        x_offset + current_level_sc.camera_where_to)
+    if $PostFightTimer.is_stopped():
+        $CameraCollision.set_destination_and_back_collision(current_level_sc.should_camera_move,
+                                                            x_offset + current_level_sc.camera_where_to)
     $MinionManager.set_minimum_number_of_minions(current_level_sc.minimum_number_of_minions)
 
     current_level_sc.global_position.x = x_offset
 
     call_deferred("add_child", current_level_sc)
+#end
+
+func _unload_level(level: Level) -> void:
+    print("Unloading level %s" % level.name)
+    call_deferred("remove_child", level)
 #end
 
 # Called when level_ended signal is triggered
@@ -69,18 +88,22 @@ func _change_level() -> void:
 
 func _on_mob_died(mob: Mob) -> void:
     if mob.is_boss():
+        current_level_sc.get_node("NiqueLaParallax").show()
+        dezoom_offset = x_offset
         $PostFightTimer.start()
+        _change_level()
 #end
 
 func _on_post_fight_timer_timeout() -> void:
-    pass
+    $CameraCollision.set_destination_and_back_collision(current_level_sc.should_camera_move,
+                                                        x_offset + current_level_sc.camera_where_to)
 #end
 
 func dezoom(delta: float) -> void:
-    var foreground: Sprite2D = current_level_sc.get_node("Foreground")
+    var foreground: Sprite2D = precedent_level_sc.get_node("Foreground")
     var zoom_target = 0.67
     var dezoom_value = delta * (1.0 - zoom_target) / $PostFightTimer.wait_time
-    var dezoom_vec = delta * (Vector2(x_offset, -360) - $CameraCollision.global_position) / $PostFightTimer.wait_time
+    var dezoom_vec = delta * (Vector2(dezoom_offset, -360) - $CameraCollision.global_position) / $PostFightTimer.wait_time
 
     foreground.scale.x -= dezoom_value
     foreground.scale.y -= dezoom_value
